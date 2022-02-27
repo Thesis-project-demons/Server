88172e6e99d06763dab1f1fd7780f7e05e7dd3c9
Mohamed-Haouach <haouachmohamed84@gmail.com>
2022-02-27 21:28:08 +0100

    auth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const userroutes = require("../controllers/user.controller.js");
 
 router.post("/signup",userroutes.Signup)
 router.post("/login",userroutes.Login)
+router.get("/getuser",userroutes.getuser)
 
 
 module.exports = router;
